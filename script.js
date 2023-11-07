6742294559f973391d75e32ed7228c9c445d996b
josephesteva <joe.esteva@gmail.com>
2023-11-07 09:19:02 -0500

    Add joke

--- script.js
+++ script.js
@@ -16,34 +16,49 @@ form.addEventListener (`submit`, async (event) => {
 	
 	const input = document.querySelector(`input`);
 	const pokeName = input.value;
-	
-	const response = await fetch (`https://pokeapi.co/api/v2/pokemon/${pokeName}`);
-	// console.log(response);
-	const pokemon = await response.json();
-	
-	console.log(pokemon);
-	// console.log(pokemon.id);
-	const list = document.querySelector(`ul`);
-	list.style.textAlign = `center`
-	
+
 	//moved code to enable replace vs list
 	const newPoke = document.querySelector(`p`);
 	const pokePic = document.querySelector(`ul > img`);
 	const message = document.querySelector(`div`)
-	// list.appendChild(newPoke);
+
+	if (pokeName == `69420`) {
+		console.log(`Test`)
+		
+		newPoke.innerText =`Hu hu hu hu hu`;
+		pokePic.src = `https://i.imgflip.com/29gymp.jpg?a471720`;
+		const list = document.querySelector(`ul`);
+		list.style.textAlign = `center`;
+		pokePic.style.margin = `10px`;
+		pokePic.style.height = `250px`;
+		message.innerText = `You're all muppets`;
+	} else {
+		const response = await fetch (`https://pokeapi.co/api/v2/pokemon/${pokeName}`);
+		// console.log(response);
+		const pokemon = await response.json();
+		
+		console.log(`Why is this not appearing`);
+		// console.log(pokemon);
+		// console.log(pokemon.id);
+		const list = document.querySelector(`ul`);
+		list.style.textAlign = `center`
+		
+		// list.appendChild(newPoke);
 	// list.appendChild(pokePic);
 
 	// for (i = 0; i <= currentList.length; i++) {
-	// 	if (currentList.includes(pokemon.name)) {
-	// 		alert(`${pokemon.name} is already in the Pokedex`)
-	// 		form.reset();
-	// 		input.focus();
-	// 		return;
+	//  if (currentList.includes(pokemon.name)) {
+		// 		alert(`${pokemon.name} is already in the Pokedex`)
+		// 		form.reset();
+		// 		input.focus();
+		// 		return;
 	// 	} else {
 			// currentList.push(pokemon.name);
 			// const newPoke = document.createElement(`p`);
 			// const pokePic = document.createElement(`img`)
+				
 			newPoke.innerText = `${pokemon.id}: ${pokemon.name.toUpperCase()}`;
+				
 			pokePic.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`
 			pokePic.style.height = `200px`
 			pokePic.style.marginTop = `-40px`
@@ -54,6 +69,7 @@ form.addEventListener (`submit`, async (event) => {
 			form.reset();
 			input.focus();
 			return;
+		}
 		// }
 	// }
 
