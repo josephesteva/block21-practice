0adb9617c039ad5219649c2ca8ddfae0b344608a
josephesteva <joe.esteva@gmail.com>
2023-11-07 08:52:50 -0500

    Change list functionality to replace after each search

--- script.js
+++ script.js
@@ -7,44 +7,55 @@
 
 const form = document.querySelector(`form`)
 
+
+
+
 const currentList = [];
 form.addEventListener (`submit`, async (event) => {
 	event.preventDefault();
-
+	
 	const input = document.querySelector(`input`);
 	const pokeName = input.value;
-
+	
 	const response = await fetch (`https://pokeapi.co/api/v2/pokemon/${pokeName}`);
 	// console.log(response);
 	const pokemon = await response.json();
-
+	
 	console.log(pokemon);
 	// console.log(pokemon.id);
 	const list = document.querySelector(`ul`);
 	list.style.textAlign = `center`
+	
+	//moved code to enable replace vs list
+	const newPoke = document.querySelector(`p`);
+	const pokePic = document.querySelector(`ul > img`);
+	const message = document.querySelector(`div`)
+	// list.appendChild(newPoke);
+	// list.appendChild(pokePic);
 
-	for (i = 0; i <= currentList.length; i++) {
-		if (currentList.includes(pokemon.name)) {
-			alert(`${pokemon.name} is already in the Pokedex`)
-			form.reset();
-			input.focus();
-			return;
-		} else {
-			currentList.push(pokemon.name);
-			const newPoke = document.createElement(`p`);
-			const pokePic = document.createElement(`img`)
+	// for (i = 0; i <= currentList.length; i++) {
+	// 	if (currentList.includes(pokemon.name)) {
+	// 		alert(`${pokemon.name} is already in the Pokedex`)
+	// 		form.reset();
+	// 		input.focus();
+	// 		return;
+	// 	} else {
+			// currentList.push(pokemon.name);
+			// const newPoke = document.createElement(`p`);
+			// const pokePic = document.createElement(`img`)
 			newPoke.innerText = `${pokemon.id}: ${pokemon.name.toUpperCase()}`;
 			pokePic.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`
 			pokePic.style.height = `200px`
 			pokePic.style.marginTop = `-40px`
-			list.appendChild(newPoke);
-			list.appendChild(pokePic);
+			message.innerText = `Are you happy now Nick? Ya muppet`
+			// list.appendChild(newPoke);
+			// list.appendChild(pokePic);
 			// console.log(currentList)
 			form.reset();
 			input.focus();
 			return;
-		}
-	}
+		// }
+	// }
 
 });
 
